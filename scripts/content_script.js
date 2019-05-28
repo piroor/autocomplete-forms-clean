2c0bbaef80920fb87d0c8fbf1d96080a32248158
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-05-28 11:19:16 +0900

    Import "AutoComplete Forms for Firefox" ver. 52.0

---
+++ scripts/content_script.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener ("load", Main, false);
+
+function Main(evt) {
+	var forms=document.querySelectorAll("form");
+	for(var i=0;i<forms.length;i++){
+		var f=forms[i];
+		f.setAttribute("autocomplete","on");
+		var els=f.elements;
+		for(var j=0;j<els.length;j++){
+			var e=els[j];
+			e.setAttribute("autocomplete","on");
+		}	
+	}
+}
